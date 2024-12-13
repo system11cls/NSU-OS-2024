1e851d61b11fb02ab1334e5bae8662efacdb63ec
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2024-12-13 08:54:46 +0700

    shell2

--- 23216/Faranosov/shell/shell.c
+++ 23216/Faranosov/shell/shell.c
@@ -23,274 +23,21 @@ char curDir[128];
 char* specCommands[] = { "jobs", "fg", "bg", "exit", "cd"};
 
 void setbgjob(job* curJob);
-
 void setfgjob(job* curJob);
-
+int setfd(streams* stream, char fdtocls);
+command* copycmd(command* cmd);
+convs* copyconv(convs* old);
+process* initProc(command* cmd);
+job* initJob(convs* conv);
+job* linkConsAndJobs();
 job* findJob(pid_t gpid);
-
+void clear(job* curj);
+void clearAll();
 void deleteJob(job* curj);
-
+void handling_status(job* curJob, int status);
+void printCurDir();
 void printJob(job* j);
-
 void printJobs();
-
-int setfd(streams* stream, char fdtocls) {
-	int fd = 0;
-	switch (fdtocls) {
-	case 0:
-		fd = open(stream->file, O_RDONLY);
-		if (fd == -1) {
-			perror("open infile error");
-			exit(1);
-		}
-		return fd;
-	case 1:
-	case 2:
-		if (stream->flags & ISCONT) {
-			fd = open(stream->file, O_WRONLY | O_APPEND | O_CREAT, 0660);
-			if (fd == -1) {
-				perror("open out/err fileAppend error");
-				exit(1);
-			}
-			return fd;
-		}
-		else {
-			fd = open(stream->file, O_WRONLY | O_CREAT, 0660);
-			if (fd == -1) {
-				perror("open out/err file error");
-				exit(1);
-			}
-			return fd;
-		}
-		break;
-	default:
-		printf("HOW???");
-		exit(1);
-	}
-}
-
-command* copycmd(command* cmd) {
-	command* newCom = NULL;
-	newCom = malloc(sizeof(command));
-	if (newCom == NULL) {
-		printf("malloc (copycmd) error\n");
-		exit(1);
-	}
-
-	newCom->cmdflag = cmd->cmdflag;
-
-
-	for (int i = 0; i < MAXARGS; i++) {
-		if (cmd->cmdargs[i] == NULL) {
-			newCom->cmdargs[i] = (char*)NULL;
-			break;
-		}
-		newCom->cmdargs[i] = NULL;
-		char* str = malloc(sizeof(char) * (strlen(cmd->cmdargs[i]) + 1));
-		strcpy(str, cmd->cmdargs[i]);
-		newCom->cmdargs[i] = str;
-		if (newCom->cmdargs[i] == NULL) {
-			printf("malloc (newCom->cmdargs[%d]) error\n", i);
-			exit(1);
-		}
-	}
-
-	return newCom;
-}
-
-convs* copyconv(convs* old) {
-	convs* newConv = NULL;
-	newConv = malloc(sizeof(convs));
-	if (newConv == NULL) {
-		printf("malloc (copyconv) error");
-		exit(1);
-	}
-
-	newConv->flag = old->flag;
-	newConv->cntcommands = old->cntcommands;
-	newConv->in.file = newConv->out.file = newConv->err.file = NULL;
-	if (old->in.file) {
-		newConv->in.file = malloc(sizeof(char) * (strlen(old->in.file) + 1));
-		if (newConv->in.file == NULL) {
-			printf("malloc (copyconv->in) error");
-			exit(1);
-		}
-		strcpy(newConv->in.file, old->in.file);
-	}
-	else newConv->in.file = NULL;
-
-	if (old->out.file) {
-		newConv->out.file = malloc(sizeof(char) * (strlen(old->out.file) + 1));
-		if (newConv->out.file == NULL) {
-			printf("malloc (copyconv->out) error");
-			exit(1);
-		}
-		strcpy(newConv->out.file, old->out.file);
-	}
-	else newConv->out.file = NULL;
-
-	if (old->err.file) {
-		newConv->err.file = malloc(sizeof(char) * (strlen(old->err.file) + 1));
-		if (newConv->err.file == NULL) {
-			printf("malloc (copyconv->err) error");
-			exit(1);
-		}
-		strcpy(newConv->err.file, old->err.file);
-	}
-	else newConv->err.file = NULL;
-
-	newConv->in.flags = old->in.flags;
-	newConv->out.flags = old->out.flags;
-	newConv->err.flags = old->err.flags;
-	return newConv;
-}
-
-process* initProc(command* cmd) {
-	process* newProc = NULL;
-	newProc = malloc(sizeof(process));
-	if (newProc == NULL) {
-		printf("malloc (initProc) error");
-		exit(1);
-	}
-	newProc->cmd = copycmd(cmd);
-	newProc->nextproc = NULL;
-	return newProc;
-}
-
-job* initJob(convs* conv) {
-	job* newjob = NULL;
-	newjob = malloc(sizeof(job));
-	if (newjob == NULL) {
-		printf("malloc initJob error");
-		exit(1);
-	}
-	newjob->state = LINKED;
-	newjob->gpid = 0;
-	newjob->conv = copyconv(conv);
-	newjob->nextjob = NULL;
-	newjob->prevjob = NULL;
-	process* curProc = NULL;
-	for (int i = 0; i < conv->cntcommands; i++) {
-		if (i == 0) {
-			curProc = initProc(&cmds[curcmd]);
-			newjob->proc = curProc;
-		}
-		else {
-			curProc->nextproc = initProc(&cmds[curcmd + i]);
-			curProc = curProc->nextproc;
-		}
-	}
-	curcmd += conv->cntcommands;
-	return newjob;
-}
-
-job* linkConsAndJobs() {
-
-	job* firstThisJob = NULL, * curJob = lastjob;
-	for (int i = 0; i < MAXCONV && conv[i].cntcommands > 0; i++) {
-		if (curJob == NULL) {
-			curJob = initJob(&conv[i]);
-			curJob->prevjob = NULL;
-			firstjob = curJob;
-			lastjob = curJob;
-		}
-		else {
-			printf("curJob != NULL\n");
-			curJob->nextjob = initJob(&conv[i]);
-			curJob->nextjob->prevjob = curJob;
-			curJob = curJob->nextjob;
-			lastjob = curJob;
-
-		}
-
-		if (curJob->conv->cntcommands != 1) {
-			for (process* p = curJob->proc; p; p = p->nextproc) {
-
-				if (strcmp(p->cmd->cmdargs[0], specCommands[3]) == 0) {
-					if (curJob->conv->cntcommands != 1) {
-						printf("Exit in conv: Undfined behavior!");
-						exit(1);
-					}
-				}
-
-			}
-
-		}
-
-
-		if (firstThisJob == NULL) {
-			firstThisJob = curJob;
-		}
-	}
-
-	
-	return firstThisJob;
-}
-
-job* findJob(pid_t gpid) {
-	job* cur = firstjob;
-	while (cur && cur->gpid != gpid) {
-		cur = cur->nextjob;
-	}
-
-	return cur;
-}
-
-void clear(job* curj) {
-	process* proc = curj->proc, * temp;
-	while (proc) {
-		temp = proc->nextproc;
-		free(proc->cmd);
-		free(proc);
-		proc = temp;
-	}
-	free(curj->conv);
-	free(curj);
-}
-
-void clearAll() {
-	for (job* j = firstjob; j != NULL; j = j->nextjob) {
-		clear(j);
-	}
-}
-
-void deleteJob(job* curj) {
-	
-	if (curj->nextjob) {
-		curj->nextjob->prevjob = curj->prevjob;
-	}
-	else {
-		lastjob = curj->prevjob;
-	}
-	if (curj->prevjob) {
-		curj->prevjob->nextjob = curj->nextjob;
-	}
-	else {
-		firstjob = curj->nextjob;
-	}
-
-
-	clear(curj);
-}
-
-void handling_status(job* curJob, int status) {
-	if (WIFEXITED(status)) {
-		if (curJob->conv->flag == BKGRND) printf("gpid = %d exited\n", curJob->gpid);
-		deleteJob(curJob);
-		return;
-	}
-	if (WIFSIGNALED(status)) {
-		printf("gpid = %d killed by signal\n", curJob->gpid);
-		deleteJob(curJob);
-		return;
-	}
-	if (WIFSTOPPED(status)) {
-		printf("gpid = %d stopped by signal\n", curJob->gpid);
-		curJob->state = STOPPED;
-		return;
-	}
-
-}
 
 void checkJobs() {
 	pid_t getted_id;
@@ -316,7 +63,6 @@ void checkJobs() {
 		}
 
 	} while (shouldCont);
-
 }
 
 void shellawaiting(job* forgjob) {
@@ -411,7 +157,7 @@ void start_job(job* jobs) {
 		if (strcmp(jobs->proc->cmd->cmdargs[0], specCommands[1]) == 0) {
 			if (jobs->proc->cmd->cmdargs[1] == NULL) {
 				job* curJ = lastjob;
-				while (curJ != NULL || curJ->conv->flag == 0) {
+				while (curJ != NULL && curJ->state != 1) {
 					curJ = curJ->prevjob;
 				}
 				if (curJ != NULL) {
@@ -582,53 +328,6 @@ void setfgjob(job* curJob) {
 	shellawaiting(curJob);
 }
 
-void printCurDir() {
-	for (int i = 0; i < 128; i++) curDir[i] = '\0';
-	getcwd(curDir, 128);
-
-	if (write(1, curDir, 128) == -1) {
-		perror("write error");
-		exit(1);
-	}
-
-	if (write(1, "> ", 2) == -1) {
-		perror("write error");
-		exit(1);
-	}
-
-}
-
-void printJob(job* j) {
-	printf("gpid = %d  ", j->gpid);
-
-	if (j->state == RUNNING) {
-		printf("Running ");
-	}
-	else if (j->state == STOPPED) {
-		printf("Stopped ");
-	}
-	else printf("Linked ");
-
-	for (process* p = j->proc; p; p = p->nextproc) {
-		for (int i = 0; p->cmd->cmdargs[i]; i++) {
-			printf("%s ", p->cmd->cmdargs[i]);
-		}
-		if (p->nextproc != NULL) printf("| ");
-	}
-	printf("\n");
-}
-
-void printJobs() {
-	printf("\n");
-	int cnt = 0;
-	for (job* j = firstjob; j; j = j->nextjob) {
-		if (j->state != 3) {
-			cnt++;
-			printJob(j);
-		}
-	}
-	printf("total: %d\n\n", cnt);
-}
 
 int main() {
 	char line[1024];
