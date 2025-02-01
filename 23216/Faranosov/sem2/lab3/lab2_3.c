f805a84b2f6225d029f9d0f4346afae74e464835
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-02-02 02:27:43 +0700

    lab2_3

--- 23216/Faranosov/sem2/lab3/lab2_3.c
+++ 23216/Faranosov/sem2/lab3/lab2_3.c
@@ -12,17 +12,18 @@ void* func(void *param) {
 	return NULL;
 }
 
+char strs1[5][3] = { "1", "2", "3", "4", NULL };
+char strs2[5][10] = { "Hi", "to", "everyone", "here", NULL };
+char strs3[5][4] = { "00", "01", "10", "11", NULL };
+char strs4[5][2] = { "a", "b", "c", "d", NULL };
 
 int main() {
 	pthread_t thread1, thread2, thread3, thread4;
 	int res;
 
-	char strs1[5][3] = {"1", "2", "3", "4", NULL};
-	char strs2[5][10] = { "Hi", "to", "everyone", "here", NULL };
-	char strs3[5][4] = { "00", "01", "10", "11", NULL };
-	char strs4[5][2] = { "a", "b", "c", "d", NULL };
+	
 
-	res = pthread_create(&thread1, NULL, func, &strs1);
+	res = pthread_create(&thread1, NULL, func, strs1);
 	if (res != 0) {
 		perror("create 1 error");
 		exit(1);
