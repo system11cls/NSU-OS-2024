c28d268cc784569a2b0d5b6a619dfd7a86b6d934
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-02-02 02:38:22 +0700

    lab2_3

--- 23216/Faranosov/sem2/lab3/lab2_3.c
+++ 23216/Faranosov/sem2/lab3/lab2_3.c
@@ -5,8 +5,8 @@
 void* func(void *param) {
 	int it = 0;
 	char** strs = param;
-	while (strs[it] != NULL) {
-		printf("%s\n", strs[it]);
+	for (int i = 0; i < 4; i++) {
+		printf("%s\n", strs[i]);
 	}
 
 	return NULL;
@@ -18,12 +18,12 @@ int main() {
 	pthread_t thread1, thread2, thread3, thread4;
 	int res;
 
-	char strs1[5][3] = { "1", "2", "3", "4", NULL };
-	char strs2[5][10] = { "Hi", "to", "everyone", "here", NULL };
-	char strs3[5][4] = { "00", "01", "10", "11", NULL };
-	char strs4[5][2] = { "a", "b", "c", "d", NULL };
+	char strs1[4][3] = { "1", "2", "3", "4"};
+	char strs2[4][10] = { "Hi", "to", "everyone", "here"};
+	char strs3[4][4] = { "00", "01", "10", "11"};
+	char strs4[4][2] = { "a", "b", "c", "d"};
 
-	res = pthread_create(&thread1, NULL, func, (void*)&strs1);
+	res = pthread_create(&thread1, NULL, func, (void*)strs1);
 	if (res != 0) {
 		perror("create 1 error");
 		exit(1);
