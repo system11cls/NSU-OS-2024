7834c4da9ed9eebb0df21ec86c8d8a267c8ce514
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-02-02 03:05:27 +0700

    lab2_3

--- 23216/Faranosov/sem2/lab3/lab2_3.c
+++ 23216/Faranosov/sem2/lab3/lab2_3.c
@@ -11,7 +11,7 @@ data d1;
 
 void* func(void *param) {
 	
-	char st[][] = param;
+	char (*st)[3] = param;
 	printf("%s\n", st[0]);
 
 	return NULL;
@@ -20,16 +20,13 @@ void* func(void *param) {
 char st[2][5] = {"abcd", "abc"};
 
 int main() {
-	pthread_t thread1, thread2, thread3, thread4;
+	pthread_t thread1;
 	int res;
 
-	const char strs1[4][3] = { "1", "2", "3", "4"};
-	
+	const char *strs1[3] = { "1", "2", "3", "4"};
 
-	d1.strs = strs1;
-	d1.cnt = 4;
 
-	res = pthread_create(&thread1, NULL, func, st);
+	res = pthread_create(&thread1, NULL, func, strs1);
 	if (res != 0) {
 		perror("create 1 error");
 		exit(1);
