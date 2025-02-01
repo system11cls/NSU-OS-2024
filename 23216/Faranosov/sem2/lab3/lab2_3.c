0f7dfb73487713304bf03af88239a7b9c01b7420
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-02-02 02:26:07 +0700

    lab2_3

--- 23216/Faranosov/sem2/lab3/lab2_3.c
+++ 23216/Faranosov/sem2/lab3/lab2_3.c
@@ -1,9 +1,10 @@
 #include "stdio.h"
-#include "stdlib.h";
+#include "stdlib.h"
 #include "pthread.h"
 
-void* func(char** strs) {
+void* func(void *param) {
 	int it = 0;
+	char** strs = (char**)param;
 	while (strs[it] != NULL) {
 		printf("%s\n", strs[it]);
 	}
@@ -21,7 +22,7 @@ int main() {
 	char strs3[5][4] = { "00", "01", "10", "11", NULL };
 	char strs4[5][2] = { "a", "b", "c", "d", NULL };
 
-	res = pthread_create(&thread1, NULL, func, strs1);
+	res = pthread_create(&thread1, NULL, func, &strs1);
 	if (res != 0) {
 		perror("create 1 error");
 		exit(1);
