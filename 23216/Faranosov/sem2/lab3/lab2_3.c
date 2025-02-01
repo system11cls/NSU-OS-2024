3518a43fb4b81768f0a7bfab3bdeb6a36f63ba96
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-02-02 02:20:29 +0700

    lab2_3

---
+++ 23216/Faranosov/sem2/lab3/lab2_3.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+#include "stdlib.h";
+#include "pthread.h"
+
+void* func(char** strs) {
+	int it = 0;
+	while (strs[it] != NULL) {
+		printf("%s\n", strs[it]);
+	}
+
+	return NULL;
+}
+
+
+int main() {
+	pthread_t thread1, thread2, thread3, thread4;
+	int res;
+
+	char strs1[5][3] = {"1", "2", "3", "4", NULL};
+	char strs2[5][10] = { "Hi", "to", "everyone", "here", NULL };
+	char strs3[5][4] = { "00", "01", "10", "11", NULL };
+	char strs4[5][2] = { "a", "b", "c", "d", NULL };
+
+	res = pthread_create(&thread1, NULL, func, strs1);
+	if (res != 0) {
+		perror("create 1 error");
+		exit(1);
+	}
+
+	pthread_exit(NULL);
+}
